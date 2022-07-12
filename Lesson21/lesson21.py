8ec5cf17dff0d517f57f085d0787a44a5708ced5
Dmitriy <nepich@gmail.com>
2022-07-12 19:17:36 +0300

    Создал роут, который выдает информацию из бд

--- Lesson21/lesson21.py
+++ Lesson21/lesson21.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import sqlite3
 
 app = Flask(__name__)
 
@@ -16,6 +17,20 @@ def second_page():
 @app.route('/3')
 def third_page():
     return 'Третья страница на FLASK и немного белеберды JK:HKLJBVJJK:BGHJKCFYJOLG'
+
+
+@app.route('/DB/<int:pk>')
+def from_db(pk):
+    connection = sqlite3.connect("lesson22_db.db")
+    cursor = connection.cursor()
+    cursor.execute('SELECT * FROM data')
+    data = cursor.fetchall()
+    if pk > len(data):
+        return 'Такого значения нет в базе данных'
+    else:
+        cursor.execute('SELECT * FROM data WHERE pk=?', (pk, ))
+        data = cursor.fetchone()
+        return f'Имя = {data[0]}, дата рождения: {data[1]}'
 
 
 app.run(debug=True)
