aaa6e0659e0dd1e13eaa2871b58023c4bdbb3d14
Dmitriy <93766405+Nepich@users.noreply.github.com>
2022-07-17 14:39:26 +0300

    Delete lesson28 directory

--- lesson28/main.py
+++
@@ -1,7 +0,0 @@
-class TreeNod:
-    def __init__(self, some_text, *kids):
-        self.__some_text = some_text
-        self.kids = kids
-
-    def get_some_text(self):
-        return self.__some_text
